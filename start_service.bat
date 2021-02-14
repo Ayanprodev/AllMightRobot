755583d0bab1698275624900e453d50b603adb01
Ayanprodev <79009881+Ayanprodev@users.noreply.github.com>
2021-02-14 10:33:46 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it 
-net stop AllMightRobot
-net start AllMightRobot
+net stop Pain bot
+net start Pain bot
